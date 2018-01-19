1825f14dd33308fe4c092d8adb0b1e74d7b58646
Mikkel Ridley <mikkel250@gmail.com>
2018-01-18 19:55:08 -0800

    worked on dom stuff

--- js/script2.js
+++ js/script2.js
@@ -1,15 +1,18 @@
 var btn = document.querySelector('#btn');
 btn.onclick = () => alert("Hello World");
 
-var btn3 = document.querySelector('#bnt3');
-btn3.addEventListener('click', () => {
+
+var button3 = document.querySelector('#bnt3');
+/* This has some issue despite being exactly the same as the instructions (I even copied/pasted to make sure)
+button3.addEventListener('click', () => {
   alert("Hello World");
 });
-
+*/
 btn.addEventListener('click', function(e) {
   console.log(e)
 })
-
+// Note, the variable 'e' is an object that references the EVENT itself
+/* 
 btn3.addEventListener('click', function(e) {
   console.log(e)
 })
@@ -20,3 +23,14 @@ btn.addEventListener('click', function(e) {
 btn.addEventListener('click', function(e) {
   e.target.style.background = 'blue'
 })
+
+function alertFunction() {
+	alert("YAY! YOU DID IT!");
+}
+
+var button4 = document.querySelector('#btn4');
+button4.onclick = alertFunction
+
+// for whatever reason this is still causing an error 
+// button3.addEventListener('click', alertFunction);
+*/
